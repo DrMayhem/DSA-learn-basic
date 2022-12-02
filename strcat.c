03396313693fe4eb35e1e5280fd8057586d9d07e
DrMayhem <96137905+DrMayhem@users.noreply.github.com>
2022-12-02 13:11:58 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,14 @@
+strcat(strt,strs);  //to concatenate OR join two strings
+
+char strt[20]="Mimansha",strs[]="Vanshika";
+int i=0,j=0;
+while(strt[i]!=0)i++;
+
+while(strs[j])
+{
+  strt[i+j]=strs[j];
+  j++;
+}
+strt[i+j]='\0';
+Footer
+© 2022 GitHub, Inc.
